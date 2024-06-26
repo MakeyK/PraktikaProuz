05d0e0b7dda07f86b0295ecac990233415fe3029
MakeyK <zevstvsrgk@gmail.com>
2024-06-26 11:43:30 +0300

    добавлены две таблицы в models, изменён пароль env

--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 const { type } = require('os')
 const sequelize = require('../db')
-const {DataTypes, DATE} = require('sequelize')
+const {DataTypes, DATE, MEDIUMINT} = require('sequelize')
 
 const Prodykt = sequelize.define('prodykt',{
     id_prodykt: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
@@ -28,15 +28,15 @@ const Orders = sequelize.define('orders', {
     id_order: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     order_list: {type: DataTypes.TEXT},
     general_amount: {type: DataTypes.INTEGER},
-    id_client: {type: DataTypes.INTEGER, unique: true}
+    id_client: {type: DataTypes.INTEGER, unique: true},
+    data_order: {type: DataTypes.DATE}
 })
 
 const Clients = sequelize.define('clients', {
     id_client: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
     FIO: {type: DataTypes.STRING},
     login: {type: DataTypes.STRING, unique: true},
-    number_phone: {type: DataTypes.TEXT, unique: true},
-    data_order: {type: DataTypes.DATE}
+    number_phone: {type: DataTypes.TEXT, unique: true}
 })
 
 const Staff = sequelize.define('staff', {
@@ -48,6 +48,16 @@ const Staff = sequelize.define('staff', {
     number_phone: {type: DataTypes.TEXT, unique: true}
 })
 
+const Produkt_Menu = sequelize.define('produkt_menu', {
+    id_produkt: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id_bluda: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+const Order_Menu = sequelize.define('order_menu', {
+    id_order: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id_bluda: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
 Prodykt.hasMany(Menu, Provider)
 Provider.belongsTo(Prodykt)
 Menu.belongsTo(Prodykt)
@@ -56,7 +66,14 @@ Menu.hasMany(Orders, Staff)
 Staff.belongsTo(Menu)
 Orders.hasMany(Clients)
 Clients.belongsTo(Orders)
+Prodykt.belongsTo(Produkt_Menu)
+Menu.belongsTo(Produkt_Menu)
+Produkt_Menu.hasMany(Menu, Prodykt)
+Orders.belongsTo(Order_Menu)
+Menu.belongsTo(Order_Menu)
+Order_Menu.hasMany(Menu, Orders)
+
 
 module.exports = {
-    Prodykt, Provider, Menu, Orders, Clients, Staff
+    Prodykt, Provider, Menu, Orders, Clients, Staff, Order_Menu, Produkt_Menu
 }
